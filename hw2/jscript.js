8b212f32f0e6b73e3e28d5e80696ca39c8c9bd5e
Shane English <shane.english@journalism.cuny.edu>
2016-02-15 14:41:01 -0500

    first commit

---
+++ hw2/jscript.js
@@ -0,0 +1,14 @@
+var six = 6;
+var twelve = 12;
+var seven = 7
+var two = 2
+
+var addition = six + twelve;
+var subtraction = twelve - six;
+var multiplication = six * seven;
+var division = six / two;
+
+document.getElementById("addition").innerHTML = "When I eat " + six + " chicken wings plus " + twelve + " chicken wings I've had " + addition + " chicken wings total.";
+document.getElementById("subtraction").innerHTML = "When I eat " + twelve + " chicken wings " + "and then throw away " + six + " chicken wings I have " + subtraction + " chicken wings left. Also, I am a monster. Who wastes perfectlly good chicken wings? ";
+document.getElementById("multiplication").innerHTML = "When I eat " + six + " chicken wings a day for " + seven + " days I've had " + multiplication + " chicken wings total.";
+document.getElementById("division").innerHTML = "When I share " + six + " chicken wings between " + two + " people we each get  " + division + " chicken wings.";
